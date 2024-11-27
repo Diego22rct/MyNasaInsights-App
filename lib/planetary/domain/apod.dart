f4f44ff93b7ac98934e198649452a30c9270ebb7
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-11-27 12:17:40 -0500

    added the domain layer

---
+++ lib/planetary/domain/apod.dart
@@ -0,0 +1,17 @@
+class Apod {
+  final String id;
+  final String copyright;
+  final String date;
+  final String explanation;
+  final String hdurl;
+  final String title;
+
+  Apod({
+    required this.id,
+    required this.copyright,
+    required this.date,
+    required this.explanation,
+    required this.hdurl,
+    required this.title,
+  });
+}
